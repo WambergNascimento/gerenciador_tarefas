41a63769196dc2e14d7a0a42661489912faca895
wamberg nascimento <wambergnascimento@gmail.com>
2023-09-24 03:25:22 -0300

    modularizacao e tratamento de e excecoes

--- gerenciador_tarefas/gerenciador_tarefas_V1.0.py
+++ gerenciador_tarefas/gerenciador_tarefas_V1.0.py
@@ -1,23 +1,58 @@
-# teste
-
 class Tarefa:
 
-    def __init__(self,lista_tarefas,titulo,descricao,status):
-        self.lista_tarefas = lista_tarefas
+    def __init__(self,titulo,descricao,status):
         self.titulo = titulo
         self.descricao = descricao
         self.status = status
 
-    def marcar_concluido(self):
-        self.status = 'concluido'
+    def marcar_concluido(self,num_tarefa):
+        if self.status == 'concluido':
+            print('Essa tarefa já foi concluida.')
+        else:
+            print(f'Tarefa {lista_tarefas[num_tarefa - 1].titulo} concluida!\n')
+            self.status = 'concluido'
 
     @classmethod
     def criar_tarefa(cls,lista_tarefas,titulo,descricao,status='pendente'):
-        tarefa = Tarefa(lista_tarefas,titulo,descricao,status)
+        tarefa = Tarefa(titulo,descricao,status)
         lista_tarefas.append(tarefa)
 
-    def remover_tarefa(self):
-        lista_tarefas.remove(self)
+    def remover_tarefa(self,num_tarefa):
+        print(f'Tarefa {lista_tarefas[num_tarefa - 1].titulo} removida.\n')
+        lista_tarefas.remove(self)\
+
+
+    @staticmethod
+    def adicionar_tarefa():
+        titulo = input('Digite o titulo da tarefa: ')
+        descricao = input('Digite a descrição da tarefa: ')
+        Tarefa.criar_tarefa(lista_tarefas, titulo, descricao)
+        print('Tarefa criada!\n')
+
+    @staticmethod
+    def listar_tarefas():
+        if lista_tarefas == []:
+            print('Não há tarefas.\n')
+        else:
+            print('Minhas Tarefas:\n')
+            for tarefa in lista_tarefas:
+                print(f'{tarefa}\n')
+
+    @staticmethod
+    def marcar_concluida():
+        num_tarefa = int(input('Digite o número da tarefa que você concluiu: '))
+        if num_tarefa < 1 or num_tarefa > len(lista_tarefas):
+            print('Essa tarefa não existe.')
+        else:
+            Tarefa.marcar_concluido(lista_tarefas[num_tarefa - 1], num_tarefa)
+
+    @staticmethod
+    def tarefa_remover():
+        num_tarefa = int(input('Digite o número da tarefa que você deseja remover: '))
+        if num_tarefa < 1 or num_tarefa > len(lista_tarefas):
+            print('Essa tarefa não existe.')
+        else:
+            Tarefa.remover_tarefa(lista_tarefas[num_tarefa - 1], num_tarefa)
 
     def __str__(self):
         return f'TAREFA {lista_tarefas.index(self)+1:02}:\nTitulo: {self.titulo}\nDescrição: {self.descricao}\nStatus: {self.status}'
@@ -29,38 +64,23 @@ Tarefa.criar_tarefa(lista_tarefas,'Comprar pão','5 pães doces')
 
 def iniciar():
     print('Bem vindo ao gerenciador de tarefas!\n\n1. Adicionar Tarefa \n2. Listar Tarefas\n3. Marcar Tarefa como Concluída\n4. Remover Tarefa \n5. Sair\n')
-    print('Teste')
     escolha = 0
-
     while escolha != 5:
         escolha = int(input('Escolha uma opção: '))
-
         if escolha < 1 or escolha > 5:
             print('Entrada inválida, tente novamente.\n')
         else:
             if escolha == 1:
-                titulo = input('Digite o titulo da tarefa: ')
-                descricao = input('Digite a descrição da tarefa: ')
-                Tarefa.criar_tarefa(lista_tarefas,titulo,descricao)
-                print('Tarefa criada!\n')
+                Tarefa.adicionar_tarefa()
             else:
                 if escolha == 2:
-                    if lista_tarefas == []:
-                        print('Não há tarefas.\n')
-                    else:
-                        print('Minhas Tarefas:\n')
-                        for tarefa in lista_tarefas:
-                            print(f'{tarefa}\n')
+                    Tarefa.listar_tarefas()
                 else:
                     if escolha == 3:
-                        num_tarefa = int(input('Digite o número da tarefa que você concluiu: '))
-                        print(f'Tarefa {lista_tarefas[num_tarefa-1].titulo} concluida!\n')
-                        Tarefa.marcar_concluido(lista_tarefas[num_tarefa-1])
+                        Tarefa.marcar_concluida()
                     else:
                         if escolha == 4:
-                            num_tarefa = int(input('Digite o número da tarefa que você deseja remover: '))
-                            print(f'Tarefa {lista_tarefas[num_tarefa-1].titulo} removida.\n')
-                            Tarefa.remover_tarefa(lista_tarefas[num_tarefa-1])
+                            Tarefa.tarefa_remover()
 
-iniciar()
-
+if __name__ == '__main__':
+    iniciar()
